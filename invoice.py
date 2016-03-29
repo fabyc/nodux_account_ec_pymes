19380d2303d25144981c8568f9aa5594619c89fd
Tatiana <tatianaq@nodux.ec>
2016-03-29 14:30:57 -0500

    Add subtotal0-12% in report

--- invoice.py
+++ invoice.py
@@ -32,6 +32,8 @@ class InvoiceReport(Report):
         invoice = records[0]
         
         localcontext['descuento'] = cls._get_descuento(Invoice, invoice)
+        localcontext['subtotal_12'] = cls._get_subtotal_12(Invoice, invoice)
+        localcontext['subtotal_0'] = cls._get_subtotal_0(Invoice, invoice)
 
         return super(InvoiceReport, cls).parse(report, records, data,
                 localcontext=localcontext)              
@@ -46,4 +48,61 @@ class InvoiceReport(Report):
             descuento = descuento + descuento_parcial
             
         return descuento
+        
+    @classmethod
+    def _get_subtotal_12(cls, Invoice, invoice):
+        subtotal0 = Decimal(0.00)
+        subtotal12 = Decimal(0.00)
+        pool = Pool()
+        Taxes1 = pool.get('product.category-customer-account.tax')
+        Taxes2 = pool.get('product.template-customer-account.tax')
+        
+        for line in invoice.lines:
+            taxes1 = Taxes1.search([('category','=', line.product.category)])
+            taxes2 = Taxes2.search([('product','=', line.product)])
+            taxes3 = Taxes2.search([('product','=', line.product.template)])
+        
+            if taxes1:
+                for t in taxes1:
+                    if str('{:.0f}'.format(t.tax.rate*100)) == '12':
+                        subtotal12= subtotal12 + (line.amount)
+            elif taxes2:
+                for t in taxes2:
+                    if str('{:.0f}'.format(t.tax.rate*100)) == '12':
+                        subtotal12= subtotal12 + (line.amount)
+            elif taxes3:
+                for t in taxes3:
+                    if str('{:.0f}'.format(t.tax.rate*100)) == '12':
+                        subtotal12= subtotal12 + (line.amount)
+                        
+            
+        return subtotal12
                  
+    @classmethod
+    def _get_subtotal_0(cls, Invoice, invoice):
+        subtotal0 = 0.00
+        subtotal12 = 0.00
+        pool = Pool()
+        Taxes1 = pool.get('product.category-customer-account.tax')
+        Taxes2 = pool.get('product.template-customer-account.tax')
+                
+        for line in invoice.lines:
+            taxes1 = Taxes1.search([('category','=', line.product.category)])
+            taxes2 = Taxes2.search([('product','=', line.product)])
+            taxes3 = Taxes2.search([('product','=', line.product.template)])
+            
+            if taxes1:
+                for t in taxes1:
+                    if str('{:.0f}'.format(t.tax.rate*100)) == '0':
+                        subtotal0= subtotal0 + (line.amount)
+            elif taxes2:
+                for t in taxes2:
+                    if str('{:.0f}'.format(t.tax.rate*100)) == '0':
+                        subtotal0= subtotal0 + (line.amount)
+                        
+            elif taxes3:
+                for t in taxes3:
+                    if str('{:.0f}'.format(t.tax.rate*100)) == '0':
+                        subtotal0= subtotal0 + (line.amount)
+                        
+        return subtotal0
