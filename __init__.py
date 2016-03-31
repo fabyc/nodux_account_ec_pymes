4fa85e5e4f6c00813ee22111f70e4c660ee6782e
Tatiana Quizhpe <tatianaq@nodux.ec>
2016-03-31 15:35:05 -0500

    Division de funcionalidades a nuevo modulo nodux_party_ec

--- __init__.py
+++ __init__.py
@@ -2,12 +2,17 @@
 #this repository contains the full copyright notices and license terms.#This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
+from .party import *
 from .invoice import *
 from .account import *
 from .product import *
+
 def register():
     Pool.register(
         Invoice,
+        Party,
+        BankAccountNumber, 
+        Company, 
         Template,
         module='nodux_account_ec_pymes', type_='model')
     Pool.register(
---
+++ party.py
@@ -0,0 +1,82 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+#! -*- coding: utf8 -*-
+from trytond.pool import *
+from trytond.model import fields
+from trytond.pyson import Eval
+from trytond.pyson import Id
+from trytond.pyson import Bool, Eval
+
+__all__ = ['Party', 'BankAccountNumber', 'Company']
+__metaclass__ = PoolMeta
+
+STATES = {
+    'readonly': ~Eval('active', True),
+    'required': True,
+}
+DEPENDS = ['active']
+
+class Party:
+    __name__ = 'party.party'
+    
+    mandatory_accounting = fields.Selection([
+            ('',''),
+            ('SI', 'Si'),
+            ('NO', 'No'),
+            ], 'Mandatory Accounting', states={
+                'invisible': Eval('type_document')!='04',
+            }
+            )  
+    contribuyente_especial = fields.Boolean(u'Contribuyente especial', states={
+            'invisible': Eval('mandatory_accounting') != 'SI',
+            }, help="Seleccione solo si es contribuyente especial"
+        )        
+    contribuyente_especial_nro = fields.Char('Nro. Resolucion', states={
+            'invisible': ~Eval('contribuyente_especial',True),
+            'required': Eval('contribuyente_especial',True),
+            }, help="Contribuyente Especial Nro.")
+    
+    vat_number = fields.Char('VAT Number', help="Value Added Tax number",
+        states={
+            'readonly': Eval('type_document')=='07',
+            'required': Bool(Eval('vat_country')),
+            },
+        depends=['active', 'vat_country'])
+        
+    type_document = fields.Selection([
+                ('', ''),
+                ('04', 'RUC'),
+                ('05', 'Cedula'),
+                ('06', 'Pasaporte'),
+                ('07', 'Consumidor Final'),
+            ], 'Type Document', states={
+                'readonly': ~Eval('active', True),
+            },  depends=['active'])
+             
+    
+class BankAccountNumber:
+    __name__ = 'bank.account.number'
+
+    @classmethod
+    def __setup__(cls):
+        super(BankAccountNumber, cls).__setup__()
+        new_sel = [
+            ('checking_account', 'Checking Account'),
+            ('saving_account', 'Saving Account'),
+        ]
+        if new_sel not in cls.type.selection:
+            cls.type.selection.extend(new_sel)
+
+
+class Company:
+    __name__ = 'company.company'
+        
+    @classmethod
+    def default_currency(cls):
+        Currency = Pool().get('currency.currency')
+        usd= Currency.search([('code','=','USD')])        
+        return usd[0].id
+            
+    @staticmethod
+    def default_timezone():
+        return 'America/Guayaquil'
